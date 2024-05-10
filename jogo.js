b33a398bbdc2b59d6faf9ed9d69b0adb42de7dd4
Ailton Daniel <ailton.developer@gmail.com>
2024-05-10 15:46:39 -0300

    Lado A e Lado B

--- jogo.js
+++ jogo.js
@@ -21,7 +21,7 @@ function posicaoRandomica(){
     //criar elemento html
     var mosquito = document.createElement('img')
     mosquito.src = 'img/mosca.png'
-    mosquito.className = tamanhoAleatorio()
+    mosquito.className = tamanhoAleatorio() + ' ' + ladoAleatorio()
     mosquito.style.left = posicaoX + 'px'
     mosquito.style.top = posicaoY + 'px'
     mosquito.style.position = 'absolute'
@@ -32,7 +32,6 @@ function posicaoRandomica(){
 
 function tamanhoAleatorio(){
     var classe = Math.floor(Math.random()*3)
-    console.log(classe)
 
     switch(classe){
         case 0:
@@ -41,5 +40,17 @@ function tamanhoAleatorio(){
             return 'mosquito2'
         case 2:
             return 'mosquito3'
+    }
+}
+
+function ladoAleatorio(){
+    var classe = Math.floor(Math.random()*2)
+
+    switch(classe){
+        case 0:
+            return 'ladoA'
+        case 1:
+            return 'ladoB'
+
     }
 }
