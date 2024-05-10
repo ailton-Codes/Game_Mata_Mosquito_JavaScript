06714abf18d8a77edbc23e3731ffe3e5ee152a88
Ailton Daniel <ailton.developer@gmail.com>
2024-05-10 18:27:01 -0300

    Vidas & Game Over

--- jogo.js
+++ jogo.js
@@ -1,5 +1,6 @@
 var altura = 0
 var largura = 0
+var vidas = 1
 
 function ajustaTamanhoPalcoJogo(){
     altura = window.innerHeight
@@ -14,6 +15,14 @@ function posicaoRandomica(){
     //remover o mosquito anterior (caso exista)
     if(document.getElementById('mosquito')){
         document.getElementById('mosquito').remove()
+
+        if(vidas>3){
+            window.location.href = 'fim_de_jogo.html'
+        }else{
+        document.getElementById('v' + vidas).src="img/coracao_vazio.png"
+
+        vidas++
+        }
     }
 
     var posicaoX = Math.floor(Math.random()*largura) - 90
@@ -32,6 +41,9 @@ function posicaoRandomica(){
     mosquito.style.top = posicaoY + 'px'
     mosquito.style.position = 'absolute'
     mosquito.id = 'mosquito'
+    mosquito.onclick = function(){
+        this.remove()
+    }
 
     document.body.appendChild(mosquito)
     
