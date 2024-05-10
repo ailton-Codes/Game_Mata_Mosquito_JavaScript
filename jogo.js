b79bd8f68275cdf988381327db62fe9b26a2b584
Ailton Daniel <ailton.developer@gmail.com>
2024-05-10 18:39:46 -0300

    Adicionando o cronometro

--- jogo.js
+++ jogo.js
@@ -1,6 +1,7 @@
 var altura = 0
 var largura = 0
 var vidas = 1
+var tempo = 10
 
 function ajustaTamanhoPalcoJogo(){
     altura = window.innerHeight
@@ -9,6 +10,17 @@ function ajustaTamanhoPalcoJogo(){
     console.log(largura, altura)
 }
 ajustaTamanhoPalcoJogo()
+
+var cronometro = setInterval(function(){
+    tempo-=1
+    if(tempo<0){
+        clearInterval(cronometro)
+        clearInterval(criaMosquito)
+        alert('Vitoria')
+    }else{
+    document.getElementById('cronometro').innerHTML = tempo
+}
+},1000)
 
 function posicaoRandomica(){
 
